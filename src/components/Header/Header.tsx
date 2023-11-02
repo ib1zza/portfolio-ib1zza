28876b7c4b77d6b78c91b869bb994edfbdf0546c
Mikhail Pshenichnyy <dremast1337@gmail.com>
2023-11-02 22:44:34 +0300

    new header fixed design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReact } from "@fortawesome/free-brands-svg-icons";
 import s from "./Header.module.scss";
-import Wrapper from "../../UI/Wrapper/Wrapper";
 import { motion } from "framer-motion";
 import Burger from "../../UI/Burger/Burger";
 import { Link, NavLink, useLocation } from "react-router-dom";
@@ -21,71 +20,46 @@ const Header = () => {
   useEffect(() => {
     setBurger(false);
   }, [pathname]);
+
   return (
-    <Wrapper>
-      <header className={s.header}>
-        <Link to={AppRoutes.HOME}>
-          <div className={s.header__logo}>
-            <div className={s.header__logo__reactContainer}>
-              <motion.div
-                className={s.react}
-                animate={{ rotate: 360 }}
-                transition={{
-                  duration: 3,
-                  ease: "linear",
-                  repeatType: "loop",
-                  repeat: Infinity,
-                }}
-              >
-                <FontAwesomeIcon icon={faReact} />{" "}
-              </motion.div>
-            </div>
-            <span>ib1zza</span>
+    <header className={s.header}>
+      <Link to={AppRoutes.HOME}>
+        <div className={s.header__logo}>
+          <div className={s.header__logo__reactContainer}>
+            <motion.div
+              className={s.react}
+              animate={{ rotate: 360 }}
+              transition={{
+                duration: 3,
+                ease: "linear",
+                repeatType: "loop",
+                repeat: Infinity,
+              }}
+            >
+              <FontAwesomeIcon icon={faReact} />{" "}
+            </motion.div>
           </div>
-        </Link>
-        <Burger onChange={setBurger} flag={burger} />
-        <div className={s.header__links}>
-          <NavLink
-            className={({ isActive }) => setStyles(isActive)}
-            to={AppRoutes.HOME}
-          >
-            {t("links.home")}
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => setStyles(isActive)}
-            to={AppRoutes.PROJECTS}
-          >
-            {t("links.works")}
-          </NavLink>
-          {/*<NavLink*/}
-          {/*  className={({ isActive }) => setStyles(isActive)}*/}
-          {/*  to={AppRoutes.MEMES}*/}
-          {/*>*/}
-          {/*  videos*/}
-          {/*</NavLink>*/}
-          {/*<NavLink*/}
-          {/*  className={({ isActive }) => setStyles(isActive)}*/}
-          {/*  to={AppRoutes.FORM}*/}
-          {/*>*/}
-          {/*  contact*/}
-          {/*</NavLink>*/}
-          {/*<NavLink*/}
-          {/*  className={({ isActive }) => setStyles(isActive)}*/}
-          {/*  to={AppRoutes.HOME}*/}
-          {/*>*/}
-          {/*  about-me*/}
-          {/*</NavLink>*/}
-          {/*<NavLink*/}
-          {/*  className={({ isActive }) => setStyles(isActive)}*/}
-          {/*  to={AppRoutes.HOME}*/}
-          {/*>*/}
-          {/*  contacts*/}
-          {/*</NavLink>*/}
-          <ChangeLang />
-          <ThemePicker />
+          <span>ib1zza</span>
         </div>
-      </header>
-    </Wrapper>
+      </Link>
+      <Burger onChange={setBurger} flag={burger} />
+      <div className={s.header__links}>
+        <NavLink
+          className={({ isActive }) => setStyles(isActive)}
+          to={AppRoutes.HOME}
+        >
+          {t("links.home")}
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => setStyles(isActive)}
+          to={AppRoutes.PROJECTS}
+        >
+          {t("links.works")}
+        </NavLink>
+        <ChangeLang />
+        <ThemePicker />
+      </div>
+    </header>
   );
 };
 
